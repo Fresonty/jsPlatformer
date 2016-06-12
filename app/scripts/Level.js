f38e1080f7eef5d9c70e4ac63145dd6f413e6097
Fresonty <leon.zamel@gmail.com>
2016-06-12 20:37:37 +0200

    Adding level

--- app/scripts/Level.js
+++ app/scripts/Level.js
@@ -1,9 +1,19 @@
 function Level() {
-    null;
+    this.level1 = [
+        ["W", "W", "S"],
+        ["S", "S", "W"]
+    ];
+    this.build = function() {
+        for (row in this.level1) {
+            for (char in this.level1[row]) {
+                new Tile(char * TILESIZE, row * TILESIZE, this.level1[row][char])
+            }
+        }
+    }
 }
 
-function Tile(x, y) {
-    this.rectangle = new PIXI.Rectangle(0, 0, 16, 16)
+function Tile(x, y, key) {
+    this.rectangle = new PIXI.Rectangle(TileKeys[key][0][0], TileKeys[key][0][1], TILESIZE, TILESIZE);
     this.texture = PIXI.loader.resources["tilesheet"].texture;
     this.texture.frame = this.rectangle;
     PIXI.Sprite.call(this, this.texture);
@@ -12,3 +22,9 @@ function Tile(x, y) {
     container.addChild(this);
 }
 Tile.prototype = Object.create(PIXI.Sprite.prototype)
+
+TileKeys = {
+    "W": [[0, 0], "Wood"],
+    "G": [[TILESIZE, 0], "Grass"],
+    "S": [[0, TILESIZE], "Stone"]
+}
