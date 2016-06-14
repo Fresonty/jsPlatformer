cacb80a76660c9da186bde5c877a565e14a8c2ab
Fresonty <leon.zamel@gmail.com>
2016-06-14 15:41:01 +0200

    Made level to string array + centered tiles

--- app/scripts/Level.js
+++ app/scripts/Level.js
@@ -1,12 +1,14 @@
 function Level() {
     this.level1 = [
-        ["W", "W", "S"],
-        ["S", "S", "W"]
+        "WWS",
+        "SSW",
+        "SSS",
+        "SSS"
     ];
     this.build = function() {
         for (row in this.level1) {
             for (char in this.level1[row]) {
-                new Tile(char * TILESIZE, row * TILESIZE, this.level1[row][char])
+                new Tile(char * TILESIZE + TILESIZE / 2, row * TILESIZE + TILESIZE / 2, this.level1[row][char])
             }
         }
     }
@@ -15,6 +17,7 @@ function Level() {
 function Tile(x, y, key) {
     this.texture = PIXI.loader.resources["./resources/img/tilesheet.json"].textures[TileKeys[key]];
     PIXI.Sprite.call(this, this.texture);
+    this.anchor.set(0.5, 0.5)
     container.addChild(this)
     this.position.x = x;
     this.position.y = y;
